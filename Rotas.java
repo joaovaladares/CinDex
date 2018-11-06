82722683d0351b490dd365c08f55dcd5187ba26e
João V. Valadares <42593399+joaovaladares@users.noreply.github.com>
2018-11-06 18:00:28 -0200

    Create Rotas.java
    Initial commit

---
+++ Rotas.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Rotas {
+
+    //Atributos exclusivos classe Rotas;
+    private String tipo;
+    private int periculosidade;
+    private Pacotes[] pacote;
+    
+    //Construtor da classe Rotas
+    public Rotas(Pacotes pacote) {
+        if (pacote.getEndereco[0] >= 0) {
+            this.periculosidade = 0;
+        } else {
+            this.periculosidade = 1;
+        }
+        if (pacote.getEndereco[1] >= 0) {
+            this.tipo = "Urbano";
+        } else {
+            this.tipo = "Rural";
+        }
+    }
+}
