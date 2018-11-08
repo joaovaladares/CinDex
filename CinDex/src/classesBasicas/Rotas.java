6af98a79c9c0d6f4aa4b42a3cbdd8fb0e6c203ba
jvvm <jvvm@cin.ufpe.br>
2018-11-08 16:48:04 -0300

    Ajeitando

--- CinDex/src/classesBasicas/Rotas.java
+++ CinDex/src/classesBasicas/Rotas.java
@@ -1,24 +1,23 @@
-package com.company;
+package classesBasicas;
 
 public class Rotas {
 
     //Atributos exclusivos classe Rotas;
     private String tipo;
     private int periculosidade;
-    private Pacotes[] pacote;
-    
-    //Construtor da classe Rotas
-    public Rotas(Pacotes pacote) {
-        if (pacote.getEndereco[0] >= 0) {
-            this.periculosidade = 0;
-        } else {
-            this.periculosidade = 1;
-        }
-        if (pacote.getEndereco[1] >= 0) {
-            this.tipo = "Urbano";
-        } else {
-            this.tipo = "Rural";
-        }
+    private double distancia;
+
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public int getPericulosidade() {
+        return this.periculosidade;
+    }
+
+    public double getDistancia() {
+        return this.distancia;
     }
 }
 
