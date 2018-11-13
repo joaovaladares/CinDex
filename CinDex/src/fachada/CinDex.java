4ddf4531997929ef686805e50d4cce8b854a16ad
Guilherme Afonso <guilherme.asp@live.com>
2018-11-13 01:44:18 -0300

    Alterações

--- CinDex/src/fachada/CinDex.java
+++ CinDex/src/fachada/CinDex.java
@@ -15,11 +15,19 @@ public class CinDex {
     private CadastroPessoa pessoas;
 
     public CinDex(boolean tipo) {
+<<<<<<< Updated upstream
         rotas = new CadastroRotas(tipo);
         veiculos = new CadastroVeiculo(3tipo);
         locais = new CadastroLocal(tipo);
         pacotes = new CadastroPacotes(tipo);
         pessoas = new CadastroPessoa(tipo);
+=======
+        this.rotas = new CadastroRotas(tipo);
+        this.veiculos = new CadastroVeiculo(tipo);
+        this.locais = new CadastroLocal(tipo);
+        this.pacotes = new CadastroPacotes(tipo);
+        this.pessoas = new CadastroPessoa(tipo);
+>>>>>>> Stashed changes
     }
 
     // Pacote
@@ -28,7 +36,43 @@ public class CinDex {
         Rota rota = pacote.getRota();
         Veiculo veiculo = pacote.getVeiculo();
 
+        if(this.rotas.existe(rota.getCodigo()) && this.veiculos.existe(veiculo.getNome())){
+            this.pacotes.cadastrar(pacote);
+        }else if(this.rotas.existe(rota.getCodigo())){
+            RotaInvalidaException e;
+            e = new RotaInvalidaException();
+            throw e;
+        }else{
+            VeiculoInvalidoException e;
+            e = new VeiculoInvalidoException();
+            throw e;
+        }
+    }
 
+    public void removerPacote(String identificador) throws PacoteNaoEncontradoException{
+        this.pacotes.remover(identificador);
+    }
+
+    public void atualizarPacote(Pacote pacote) throws PacoteNaoEncontradoException,
+            RotaInvalidaException, VeiculoInvalidoException{
+        Rota rota = pacote.getRota();
+        Veiculo veiculo = pacote.getVeiculo();
+
+        if(this.rotas.existe(rota.getCodigo()) && this.veiculos.existe(veiculo.getNome())){
+            this.pacotes.atualizar(pacote);
+        }else if(this.rotas.existe(rota.getCodigo())){
+            RotaInvalidaException e;
+            e = new RotaInvalidaException();
+            throw e;
+        }else{
+            VeiculoInvalidoException e;
+            e = new VeiculoInvalidoException();
+            throw e;
+        }
+    }
+
+    public void procurarPacote(String identificador) throws PacoteNaoEncontradoException{
+        this.pacotes.procurar(identificador)
     }
 }
 
